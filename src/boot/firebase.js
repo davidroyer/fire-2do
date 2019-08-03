2dbf143e17b612045c93f83dae2b03916b13753d
David Royer <droyer01@gmail.com>
2019-08-03 00:45:55 -0400

    feat(firebase): adds firebase image upload

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,8 +1,8 @@
-import * as firebase from "firebase/app"
+import * as firebase from "firebase/app";
 
-import "firebase/auth"
-import "firebase/database"
-
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
 
 // ADD YOUR FIREBASE CONFIG OBJECT HERE:
 var firebaseConfig = {
@@ -13,10 +13,11 @@ var firebaseConfig = {
   storageBucket: "fire-2do-app.appspot.com",
   messagingSenderId: "572723294876",
   appId: "1:572723294876:web:63216a1828f50dab"
-}
+};
 
-let firebaseApp = firebase.initializeApp(firebaseConfig)
-let firebaseAuth = firebaseApp.auth()
-let firebaseDb = firebaseApp.database()
+let firebaseApp = firebase.initializeApp(firebaseConfig);
+let firebaseAuth = firebaseApp.auth();
+let firebaseDb = firebaseApp.database();
+let firebaseStorage = firebaseApp.storage();
 
-export { firebaseAuth, firebaseDb }
+export { firebaseAuth, firebaseDb, firebaseStorage };
--- src/pages/PageSettings.vue
+++ src/pages/PageSettings.vue
@@ -1,115 +1,124 @@
 <template>
   <q-page padding>
-		<q-list
-			class="q-mb-md"
-			bordered
-			padding>
-		  <q-item-label header>Settings</q-item-label>
+    <q-list class="q-mb-md" bordered padding>
+      <q-item-label header>Settings</q-item-label>
 
-		  <q-item tag="label" v-ripple>
-		    <q-item-section>
-		      <q-item-label>Show 12 hour time format</q-item-label>
-		    </q-item-section>
-		    <q-item-section side >
-		      <q-toggle
-		      	v-model="show12HourTimeFormat"
-		      	color="blue" />
-		    </q-item-section>
-		  </q-item>
+      <q-item tag="label" v-ripple>
+        <q-item-section>
+          <q-item-label>Show 12 hour time format</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-toggle v-model="show12HourTimeFormat" color="blue" />
+        </q-item-section>
+      </q-item>
 
-		  <q-item tag="label" v-ripple>
-		    <q-item-section>
-		      <q-item-label>Show tasks in one list</q-item-label>
-		    </q-item-section>
-		    <q-item-section side >
-		      <q-toggle
-		      	v-model="showTasksInOneList"
-		      	color="blue" />
-		    </q-item-section>
-		  </q-item>
+      <q-item tag="label" v-ripple>
+        <q-item-section>
+          <q-item-label>Show tasks in one list</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-toggle v-model="showTasksInOneList" color="blue" />
+        </q-item-section>
+      </q-item>
+    </q-list>
 
-		</q-list>
+    <q-list bordered padding>
+      <q-item-label header>More</q-item-label>
 
-		<q-list bordered padding>
-		  <q-item-label header>More</q-item-label>
+      <q-item to="/settings/help" tag="label" v-ripple>
+        <q-item-section>
+          <q-item-label>Help</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-icon name="chevron_right" />
+        </q-item-section>
+      </q-item>
 
-		  <q-item
-		  	to="/settings/help"
-		  	tag="label"
-		  	v-ripple>
-		    <q-item-section>
-		      <q-item-label>Help</q-item-label>
-		    </q-item-section>
-		    <q-item-section side >
-		      <q-icon name="chevron_right" /> 
-		    </q-item-section>
-		  </q-item>
+      <q-item @click="visitOurWebsite" tag="label" v-ripple>
+        <q-item-section>
+          <q-item-label>Visit our website</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-icon name="chevron_right" />
+        </q-item-section>
+      </q-item>
 
-		  <q-item
-		  	@click="visitOurWebsite"
-		  	tag="label"
-		  	v-ripple>
-		    <q-item-section>
-		      <q-item-label>Visit our website</q-item-label>
-		    </q-item-section>
-		    <q-item-section side >
-		      <q-icon name="chevron_right" /> 
-		    </q-item-section>
-		  </q-item>
+      <q-item @click="emailUs" tag="label" v-ripple>
+        <q-item-section>
+          <q-item-label>Email us</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <q-icon name="chevron_right" />
+        </q-item-section>
+      </q-item>
+    </q-list>
 
-		  <q-item
-		  	@click="emailUs"
-		  	tag="label"
-		  	v-ripple>
-		    <q-item-section>
-		      <q-item-label>Email us</q-item-label>
-		    </q-item-section>
-		    <q-item-section side >
-		      <q-icon name="chevron_right" /> 
-		    </q-item-section>
-		  </q-item>
-
-		</q-list>
-
+    <q-input @input="val => { file = val[0] }" filled type="file" hint="Native file" />
+    <img v-if="lastUploadUrl" :src="lastUploadUrl" alt />
   </q-page>
 </template>
 
 <script>
-	import { mapGetters, mapActions } from 'vuex'
-	import { openURL } from 'quasar'
+import { mapGetters, mapActions } from 'vuex'
+import { openURL } from 'quasar'
+import { firebaseStorage } from 'boot/firebase'
 
-	export default {
-		computed: {
-			...mapGetters('settings', ['settings']),
-			show12HourTimeFormat: {
-				get() {
-					return this.settings.show12HourTimeFormat
-				},
-				set(value) {
-					this.setShow12HourTimeFormat(value)
-				} 
-			},
-			showTasksInOneList: {
-				get() {
-					return this.settings.showTasksInOneList
-				},
-				set(value) {
-					this.setShowTasksInOneList(value)
-				} 
-			}
-		},
-		methods: {
-			...mapActions('settings', ['setShow12HourTimeFormat', 'setShowTasksInOneList']),
-			visitOurWebsite() {
-				openURL('http://www.google.com')
-			},
-			emailUs() {
-				window.location.href = 'mailto:hello@awesometodo?subject=Awesome Todo Feedback'
-			}
-		}
-	}
+export default {
+  data: () => ({
+    file: null,
+    files: null,
+    lastUploadUrl: null
+  }),
+  watch: {
+    file (file) {
+      console.log('file choosen: ', file);
+      const { name } = file
+      const storageRef = firebaseStorage.ref();
+      storageRef.child(`images/${name}`).put(file)
+        // .then(snapshot => snapshot.ref.getDownloadURL())
+        .then(snapshot => {
+          console.log('snapshot value: ', snapshot);
+
+          return snapshot.ref.getDownloadURL()
+        })
+        .then((url) => {
+          console.log(url);
+          this.lastUploadUrl = url
+        })
+        .catch(console.error);
+
+    }
+  },
+  computed: {
+    ...mapGetters('settings', ['settings']),
+    show12HourTimeFormat: {
+      get () {
+        return this.settings.show12HourTimeFormat
+      },
+      set (value) {
+        this.setShow12HourTimeFormat(value)
+      }
+    },
+    showTasksInOneList: {
+      get () {
+        return this.settings.showTasksInOneList
+      },
+      set (value) {
+        this.setShowTasksInOneList(value)
+      }
+    }
+  },
+  methods: {
+    ...mapActions('settings', ['setShow12HourTimeFormat', 'setShowTasksInOneList']),
+    visitOurWebsite () {
+      openURL('http://www.google.com')
+    },
+    emailUs () {
+      window.location.href = 'mailto:hello@awesometodo?subject=Awesome Todo Feedback'
+    }
+  }
+}
 </script>
 
 <style>
-	
 </style>
